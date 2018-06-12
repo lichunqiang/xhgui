7cc303cea5ef78b802afa68cad2a535e4c2ae433
lichunqiang <light-li@hotmail.com>
2018-06-12 08:33:36 +0000

    [feat] Recevier support

--- swoole.php
+++ swoole.php
@@ -4,7 +4,6 @@ require __DIR__ . '/src/bootstrap.php';
 $server = new \Swoole\Server('0.0.0.0', 9501, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
 $server->set([
-    'daemonize' => 1,
     'worker_num' => 4,
     'log_file' => __DIR__ . '/cache/swoole.log',
     'pid_file' => __DIR__ . '/cache/swoole.pid',
@@ -14,11 +13,7 @@ $server->set([
 
 $server->on('packet', function ($serv, $data, $client_info) {
     //debug the client info.
-    error_log(
-        sprintf('[%s] %s' . PHP_EOL, date('Y-m-d H:i:s'), http_build_query($client_info, '', '|')),
-        3,
-        __DIR__ . '/cache/client.log'
-    );
+    echo sprintf('[%s] %s' . PHP_EOL, date('Y-m-d H:i:s'), http_build_query($client_info, '', '|'));
     try {
         $config = Xhgui_Config::all();
         $config += ['db.options' => []];
@@ -29,5 +24,5 @@ $server->on('packet', function ($serv, $data, $client_info) {
     }
 });
 
-
+echo 'Starting UDP Server: 0.0.0.0:9501' . PHP_EOL;
 $server->start();
