54b080cc6f266d820a104af552240b3aa4c81522
__FresHmaN <light-li@hotmail.com>
2018-08-27 16:56:28 +0800

    [feat] enhance logic

--- swoole.php
+++ swoole.php
@@ -4,6 +4,7 @@ require __DIR__ . '/src/bootstrap.php';
 $server = new \Swoole\Server('0.0.0.0', 9501, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
 $server->set([
+    'daemonize' => 1,
     'worker_num' => 4,
     'log_file' => __DIR__ . '/cache/swoole.log',
     'pid_file' => __DIR__ . '/cache/swoole.pid',
@@ -13,14 +14,20 @@ $server->set([
 
 $server->on('packet', function ($serv, $data, $client_info) {
     //debug the client info.
+    $clientInfo = http_build_query($client_info, '', '|');
     echo sprintf('[%s] %s' . PHP_EOL, date('Y-m-d H:i:s'), http_build_query($client_info, '', '|'));
+    error_log(date('Y-m-d H:i:s') . $clientInfo . PHP_EOL, 3, __DIR__ . '/cache/client.log');
     try {
         $config = Xhgui_Config::all();
         $config += ['db.options' => []];
         $saver = Xhgui_Saver::factory($config);
         $saver->save(json_decode($data, true));
     } catch (Exception $e) {
-        error_log($e->getMessage() . PHP_EOL, 3, __DIR__ . '/cache/error.log');
+        error_log(
+            sprintf('[%s] %s. clientInfo: %s. data: %s', date('Y-m-d H:i:s'), $e->getMessage(), $clientInfo, $data) . PHP_EOL,
+            3,
+            __DIR__ . '/cache/error.log'
+        );
     }
 });
 
